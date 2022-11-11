f53427f230737531958ad1a9d368ea6eebd5fe5a
molim <mohade3.karbalaei@gmail.com>
2022-11-11 14:23:39 +0100

    remove bugs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
   const [nav, setNav] = useState(false);
 
   // any time we click on the hamburger menu, we want to toggle the nav
-  const handleNave = () => {
+  const handleNav = () => {
     setNav(!nav);
   };
   return (
@@ -20,30 +20,25 @@ const Navbar = () => {
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div className="block md:hidden">
+      <div onClick={handleNav} className="block md:hidden">
         {/* toggle between 2 icons: */}
-        {!nav ? (
-          <AiOutlineClose size={20} onClick={handleNave} />
-        ) : (
-          <AiOutlineMenu onClick={handleNave} />
-        )}
+        {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu />}
       </div>
-      <div
+      <ul
         className={
-          !nav
-            ? "fixed left-0 top-0 w-[60%] border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500"
-            : "fixed left-[-100%]"
+          nav
+            ? "fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500"
+            : "ease-in-out duration-500 fixed left-[-100%]"
         }
       >
         <h1 className="text-3xl font-bold w-full m-8 text-[#0d9a]">Data.</h1>
-        <ul className="p-4 uppercase">
-          <li className="p-4 border-b border-gray-600">Home</li>
-          <li className="p-4 border-b border-gray-600">Company</li>
-          <li className="p-4 border-b border-gray-600">Resources</li>
-          <li className="p-4 border-b border-gray-600">About</li>
-          <li className="p-4 border-b border-gray-600">Contact</li>
-        </ul>
-      </div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">Data.</h1>
+        <li className="p-4 border-b border-gray-600">Home</li>
+        <li className="p-4 border-b border-gray-600">Company</li>
+        <li className="p-4 border-b border-gray-600">Resources</li>
+        <li className="p-4 border-b border-gray-600">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
     </div>
   );
 };
