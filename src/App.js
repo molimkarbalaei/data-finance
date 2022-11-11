8c799f4574e7997548ab4069d84020807e410864
molim <mohade3.karbalaei@gmail.com>
2022-11-11 15:35:08 +0100

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
-    <div className="text-white">
+    <div>
       <Navbar />
+      <Hero />
+      <Analytics />
+      <Newsletter />
+      <Cards />
+      <Footer />
     </div>
   );
 }
